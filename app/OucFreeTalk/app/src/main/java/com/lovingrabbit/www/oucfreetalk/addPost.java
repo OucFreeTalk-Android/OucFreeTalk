ef57b040399d1212429e74de80f47c3590adf3fd
zjkzjk <179226997@qq.com>
2017-11-10 21:55:36 +0800

    git

--- app/OucFreeTalk/app/src/main/java/com/lovingrabbit/www/oucfreetalk/addPost.java
+++ app/OucFreeTalk/app/src/main/java/com/lovingrabbit/www/oucfreetalk/addPost.java
@@ -1,8 +1,118 @@
 package com.lovingrabbit.www.oucfreetalk;
 
-/**
- * Created by zjk on 2017/10/31.
- */
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.content.SharedPreferences;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class addPost {
+import com.lovingrabbit.www.oucfreetalk.untils.AddPostAysncTaskLoader;
+
+import org.json.JSONObject;
+
+public class AddPost extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>{
+    String title,content;
+    String ADD_POST_URL = "http://47.93.222.179/oucfreetalk/addPost";
+    LoaderManager loaderManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addpost);
+        LinearLayout post = (LinearLayout) findViewById(R.id.addpost_post);
+        final LinearLayout find = (LinearLayout) findViewById(R.id.addpost_find);
+        LinearLayout addPost = (LinearLayout) findViewById(R.id.addpost_add_post);
+        LinearLayout notice = (LinearLayout) findViewById(R.id.addpost_notice);
+        LinearLayout set = (LinearLayout) findViewById(R.id.addpost_set);
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                loaderManager.restartLoader(0,null, MainActivity.this);
+                Intent intent = new Intent(AddPost.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        addPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Button addpostBtn = (Button) findViewById(R.id.add);
+        addpostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEd = (EditText) findViewById(R.id.addpost_title);
+                EditText contentEd = (EditText) findViewById(R.id.addpost_content);
+                title = titleEd.getText().toString();
+                content = contentEd.getText().toString();
+                loaderManager = getLoaderManager();
+
+                // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                // because this activity implements the LoaderCallbacks interface).
+                loaderManager.initLoader(0, null, AddPost.this);
+            }
+        });
+        ImageView re_btn = (ImageView) findViewById(R.id.return_btn);
+        re_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        SharedPreferences sharedPreferences = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("id","");
+        if (username == ""){
+            Toast.makeText(this,"请登录！",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(AddPost.this,Login.class);
+        }else {
+            return new AddPostAysncTaskLoader(this,username,title,content,ADD_POST_URL);
+        }
+        return null;
+    }
+    private int parseJson(String JSONData) {
+        int results = 100;
+        try {
+            Log.e("zjkzjk:",JSONData);
+
+            JSONObject jsonObject = new JSONObject(JSONData);
+            results = jsonObject.getInt("result");
+            Log.e("result:",results+"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        int log_results = parseJson(data);
+
+        if (log_results == 0 ){
+            Toast.makeText(AddPost.this,"发表失败",Toast.LENGTH_SHORT).show();
+        }else if (log_results == 1 ){
+            Toast.makeText(AddPost.this,"发表成功",Toast.LENGTH_SHORT).show();
+            finish();
+        }else if (log_results == 2 ){
+            Toast.makeText(AddPost.this,"服务器故障",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
 }
